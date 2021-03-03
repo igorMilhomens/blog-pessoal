874772ef8efd98a17edee405f0ce48684525b00b
Igor M <iggorm@yahoo.com.br>
2021-03-03 11:30:36 -0300

    fix: correção de cadastro, impedindo usuario duplicado #8

--- blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -17,14 +17,23 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 	
-	public Usuario CadastrarUsuario(Usuario usuario) {
+	//VALIDAÇÃO PARA NÃO PERMITIR CRIAÇÃO DE USUARIO EXISTENTE
+	
+	//public Usuario CadastrarUsuario(Usuario usuario) {
+	
+	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
+		
+		
+		if(repository.findByUsuario(usuario.getUsuario()).isPresent()) 
+			return null;
+		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
 		String senhaEncoder =  encoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaEncoder);
 		
-		return repository.save(usuario);
-		
+		//return repository.save(usuario);
+		return Optional.of(repository.save(usuario));
 	}
 	
 	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
